b4c6c1778865d7ec34b337414882420c7d5a1ea8
icct-emulholland <91078342+icct-emulholland@users.noreply.github.com>
2025-06-18 13:27:00 +0200

    Added year and quarter as variables to main

--- code/main.py
+++ code/main.py
@@ -6,10 +6,13 @@ Additional figures: a.musa@theicct.org ; Supervisor: Hussein Basma
 """
 
 import sys
-import os
 
 # Define the path to the code directory
-code_directory = r"/Users/e.mulholland/Documents/Work 2025/EU/RtZ/Q1/RtZ_formatter_folder2025/code"
+folder_location = r"/Users/e.mulholland/Documents/Work 2025/EU/RtZ/Q1/RtZ_formatter_folder2025"
+code_directory = folder_location + "/code"
+input_file_name = "Dataforce ICCT CV 2025Q1.csv"
+year = "2025"
+quarter = "Q1"
 
 # Add the code directory to the system path
 sys.path.append(code_directory)
@@ -21,28 +24,22 @@ import bus_manual_correct
 import processing
 
 
-def main():
-    folder_location = r"/Users/e.mulholland/Documents/Work 2025/EU/RtZ/Q1/RtZ_formatter_folder2025"
-    
-    preprocessing.run_preprocessing(folder_location, "Dataforce ICCT CV 2025Q1.csv", "Dataforce_Q1_2025.csv")
-    
-    
+def main():    
+    preprocessing.run_preprocessing(folder_location, input_file_name, "Dataforce_" +quarter+"_"+year+".csv")
     axle_mapping.axle_gvw_maps(
         RtZ_folder_location=folder_location,
-        input_file_new="Dataforce_Q1_2025.csv",
+        input_file_new="Dataforce_" +quarter+"_"+year+".csv",
         input_file_old="IHS_annual_master.csv",
         gvw_map_file="gvw_map_index.csv",
-        output_file="Dataforce_Q1_2025_axle_corrected.csv")
+        output_file="Dataforce_" +quarter+"_"+year+"_axle_corrected.csv")
     
     bus_assignment.bus_type_assignment(folder_location, "bus_types.csv",
-                      "Dataforce_Q1_2025.csv", 
-                      "bus_sales_with_types_dataforce_Q1.csv")
-    bus_manual_correct.correct_bus_type_classification(folder_location, "bus_sales_with_types_dataforce_Q1.csv", "bus_sales_corrected_Q1.csv")
-
+                      "Dataforce_" +quarter+"_"+year+".csv", 
+                      "bus_sales_with_types_dataforce_"+year+"_"+quarter+".csv")
     
-    processing.sorting_code(folder_location, "2025", "Q1","/outputs/Dataforce_Q1_2025_formatted_data.xlsx")
+    bus_manual_correct.correct_bus_type_classification(folder_location, "bus_sales_with_types_dataforce_"+year+"_"+quarter+".csv", "bus_sales_corrected_"+year+"_"+quarter+".csv")
+    processing.sorting_code(folder_location, year, quarter,"/outputs/Dataforce_"+quarter+"_"+year+"_formatted_data.xlsx")
     
 
 if __name__ == "__main__":
     main()
-
